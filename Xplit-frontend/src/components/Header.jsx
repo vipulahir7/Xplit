f0e95d9bc67517412ae937af5c961d8a2d1fa8be
ahir vipul <vipulahir9824@gmail.com>
2024-05-22 14:40:16 +0530

    Used data attributes to change colors

--- Xplit-frontend/src/components/Header.jsx
+++ Xplit-frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header(){
     const { isDark } = useContext(GlobalContext);
 
     return(
-        <header className={`flex ${isDark? "bg-[#1F2937] text-white" : "bg-[#d0d0d0] text-black"} items-center justify-center w-[100vw] h-[10vh]`} >
+        <header className={`flex ${isDark? "bg-[rgb(31,41,55)] text-white" : "bg-[#d0d0d0] text-black"} items-center justify-center w-[100vw] h-[10vh]`} >
             <div className="flex items-center justify-between w-[80vw] h-[100%]">
                 <Logo />
                 <div className="flex w-[15vw] justify-between h-[100%] items-center">
