3a60647f1d8d6c844cb21504d98242f489799628
ahir vipul <vipulahir9824@gmail.com>
2024-05-22 14:40:46 +0530

    data attributes to change theme

--- Xplit-frontend/src/components/Navigator.jsx
+++ Xplit-frontend/src/components/Navigator.jsx
@@ -8,8 +8,8 @@ export default function Navigator(){
     const { isDark } = useContext(GlobalContext);
 
     return (
-        <nav className={`${isDark? "bg-[#111827] text-white" : "bg-[#ffffff] text-black"} w-[100vw] h-[10vh] flex justify-center`} >
-            <div className={`${isDark? "bg-[#374151] text-white" : "bg-[#d0d0d0] text-black"} w-[21vw] h-[8vh] rounded-lg flex justify-evenly`}>
+        <nav className="bg-[color:var(--primary-bg)] w-[100vw] h-[10vh] flex justify-center">
+            <div className="bg-[color:var(--nav-bg)] w-[21vw] h-[8vh] rounded-lg flex justify-evenly">
                 <NavigatorButton src={`${isDark ? "../../images/expense-white.svg" : "../../images/expense-black.svg"}`} alt="expense"/>
                 <NavigatorButton src={`${isDark ? "../../images/transact-white.svg" : "../../images/transact-black.svg"}`} alt="expense"/>
                 <NavigatorButton src={`${isDark ? "../../images/setting-white.svg": "../../images/setting-black.svg"}`} alt="expense"/>
