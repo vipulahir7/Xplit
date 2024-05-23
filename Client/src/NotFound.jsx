9bae07a2cad3c66c30fa8a249696480003b78232
NakulRathod18 <nakulrathod8586@gmail.com>
2024-05-23 18:02:27 +0530

    Error page and SVG

---
+++ Client/src/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NotFound(){
+  return (
+    <div className="flex flex-col items-center justify-center h-[100%] bg-[color:var(--primary-bg)]">
+      <h1 className="text-6xl font-bold mb-4 animate-pulse ">404</h1>
+      <p className="text-2xl mb-8">Page Not Found</p>
+      <Link to="/" className="text-blue-500 hover:underline text-lg">
+        Go back to Home
+      </Link>
+    </div>
+  );
+};
+
--- Client/src/welcome/Message.jsx
+++ Client/src/welcome/Message.jsx
@@ -1,6 +1,6 @@
 import Button from '../components/Button';
 import { Link } from 'react-router-dom';
-import '../index.css'
+import '../index.css'  
 
 export default function Massage() {
     return (
