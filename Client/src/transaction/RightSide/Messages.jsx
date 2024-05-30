2f8ce025ac94a924dafff077fbd1487b40795147
NakulRathod18 <nakulrathod8586@gmail.com>
2024-05-30 18:21:50 +0530

    messages css

---
+++ Client/src/transaction/RightSide/Messages.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Message from "./Message";
+
+export default function Messages(){
+    return(
+        <div className="usermessage h-[86%] overflow-x-hidden overflow-y-auto">
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+            <Message />
+    
+        </div>
+    )
+}
--- Client/src/transaction/RightSide/Right.jsx
+++ Client/src/transaction/RightSide/Right.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ChatUser from "./ChatUser";
-import Message from "./Message";
+import Messages from "./Messages";
 
 export default function Right(){
     return(
         <div className="w-[40vw] h-[78vh] rounded-lg m-2 bg-[color:var(--nav-bg)]">
-            <ChatUser/>
-            <Message />
+            <ChatUser />
+            <Messages />
         </div>
     )
 }
