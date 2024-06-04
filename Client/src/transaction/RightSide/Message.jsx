a608b6c331acf30e0680f87eb8577e69e5d0bae2
NakulRathod18 <nakulrathod8586@gmail.com>
2024-06-04 15:29:43 +0530

    design

--- Client/src/transaction/RightSide/Message.jsx
+++ Client/src/transaction/RightSide/Message.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 export default function Message({time,isLeft,amount,note}){
     return(
-        <div>
-             <div className={`${isLeft ?"justify-start ml-3" :"justify-end mr-3" } flex `}>
-                <div className="bg-[color:var(--primary-btn)] text-lg p-2 rounded-lg">{amount}<span>{note}</span></div>
+           <div>
+             <div className={`${isLeft ?"justify-start ml-3" :"justify-end mr-3" } flex m-1`}>
+                <div className="flex flex-col">
+                    <div className="bg-[color:var(--primary-btn)] text-lg text-center rounded-t-lg">
+                        {amount}
+                    </div>
+                    <div className="bg-[color:var(--primary-btn)] text-lg px-2 rounded-b-md">
+                        {note}
+                    </div>
+                </div>
             </div>
+            <div className={`${isLeft ?"justify-start ml-3" :"justify-end mr-3" } flex text-xs`}>{time}</div>
+            
         </div>
     )
 }
