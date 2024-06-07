0c26a6967a42624d58777035c737b7426ba8fe79
NakulRathod18 <nakulrathod8586@gmail.com>
2024-06-07 12:49:55 +0530

    css

--- Client/src/expense/monthly/monthlyList.jsx
+++ Client/src/expense/monthly/monthlyList.jsx
@@ -1,8 +1,8 @@
-export default function MonthlyList({date,amount}){
+export default function MonthlyList({ date, amount }) {
     return (
-        <div className="flex justify-around items-center rounded-md bg-[color:var(--notes-secondary-bg)] mt-2 mb-2 w-[90%]">
-            <span>{date}</span>
-            <span>{amount}</span>
+        <div className="monthly-item flex justify-between items-center rounded-md bg-[color:var(--notes-secondary-bg)] mt-2 mb-2 w-[90%] p-4 transition-all duration-300 hover:bg-[color:var(--notes-hover-bg)] hover:scale-105 hover:text-black">
+            <span className="date text-lg font-medium">{date}</span>
+            <span className="amount text-lg font-semibold">{amount}</span>
         </div>
     )
 }
